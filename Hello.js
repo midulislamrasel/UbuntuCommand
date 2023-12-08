1ab8c435e160a6b954b901b99a4ad489e1d68f27
midulislamrasel <midulraddy121@gmail.com>
2023-12-08 15:26:56 +0600

    Add

---
+++ Hello.js
@@ -0,0 +1,15 @@
+ Ubuntu থেকে কোনো ওয়েবসাইটে ব্লক করে রাখার জন্য command
+   sudo nano /etc/hosts
+   
+ Ubuntu থেকে সফটেয়ারস আপডেট command
+   sudo apt update
+   sudo apt upgrade
+   sudo apt-get update
+   sudo apt-get install aptitude -y
+   sudo aptitude safe-upgrade
+
+ Ubuntu ফোল্ডার তৈরি
+   mkdir FolderName
+
+ Ubuntu ফাইল তৈরি
+   gedit bar.txt
